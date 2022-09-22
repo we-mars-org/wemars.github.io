f6544ee390d6080ec85a7eec9887c480d78a0c1d
Dominic Guerra <dominic.guerra81@yahoo.ca>
2022-09-22 15:50:53 -0400

    Header w/ dropdown functionality added

--- app.js
+++ app.js
@@ -6,19 +6,27 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "source/public"), {index: false}));
 
-const port = 3000
+const port = 3000;
 const public_ref = path.join(__dirname, "/source/public");
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(public_ref, "/homepage.html"))
-  console.log('get request made');
+  console.log('get request made \t' + log_time());
 })
 
-/*app.get('/style.css', (req, res) => {
-    res.sendFile(path.join(public_ref, "/style.css"));
-    console.log("stylesheet request");
-})*/
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(public_ref, "/about.html"));
+  console.log("get to about page");
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function log_time(){
+  let time = new Date();
+  let seconds = time.getSeconds();
+  let minutes = time.getMinutes();
+  let hours   = time.getHours();
+  return (hours + ":" + minutes + ":" + seconds);
+}
